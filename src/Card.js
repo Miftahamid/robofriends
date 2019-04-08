435e792d65199e0736e41eba22543334256cb19e
Mifta Hamid <miftahamid@Miftas-MacBook-Air.local>
2019-04-08 17:30:34 -0400

    Created a card and they can hover next make it in the same line

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <div className="bg-light-green dib br3 pa3 ma2 grow">
+        <img alt="photo" src="https://robohash.org/Mifta" />
+      </div>
+      <h2>Jane Doe</h2>
+      <p>jandoe@gmail.com</p>
+    </div>
+  );
+};
+export default Card;
